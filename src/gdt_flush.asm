252a57de7a546b230ef0e1140fded5522d231eba
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2025-06-23 22:43:00 -0300

    correção de Kernel Panic

--- src/gdt_flush.asm
+++ src/gdt_flush.asm
@@ -2,9 +2,10 @@ section .text
 global gdt_flush
 
 gdt_flush:
-    mov eax, [esp + 4]  ; pega ponteiro para gdt_ptr
-    lgdt [eax]          ; carrega o GDTR
+    mov eax, [esp + 4]
+    lgdt [eax]
 
+    ; Carrega os segmentos de dados
     mov ax, 0x10
     mov ds, ax
     mov es, ax
@@ -12,6 +13,12 @@ gdt_flush:
     mov gs, ax
     mov ss, ax
 
+    ; Importante! Corrigir o ESP após trocar o SS
+    ; Define uma pilha temporária segura (por exemplo, no topo da BSS)
+    extern _stack_end
+    mov esp, _stack_end
+
+    ; Faz o far jump para carregar o novo CS
     jmp 0x08:.flush_end
 
 .flush_end:
